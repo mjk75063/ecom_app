609e97c985cffea6ec5ef9c8735b3945b4f1bba6
Matt Krueger <matt@xceleratedconsulting.com>
2018-04-25 04:45:29 +0000

    Added bootstrap gem

--- config/routes.rb
+++ config/routes.rb
@@ -1,5 +1,6 @@
 Rails.application.routes.draw do
   root to: 'pages#home' 
+  # GET request to /about,  rails goes to pages controller, looks up 'about' action (AKA METHOD/FUNCTION)
   get 'about', to: 'pages#about'
 
 end
