fb10a409793fb592c8e6fe968782736a561219d1
Matt Krueger <matt@xceleratedconsulting.com>
2018-04-25 19:01:41 +0000

    Added contact controller and view

--- config/routes.rb
+++ config/routes.rb
@@ -2,5 +2,5 @@ Rails.application.routes.draw do
   root to: 'pages#home' 
   # GET request to /about,  rails goes to pages controller, looks up 'about' action (AKA METHOD/FUNCTION)
   get 'about', to: 'pages#about'
-
+  resources :contacts
 end
