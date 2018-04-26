c11cf0ebb057881c72da849c0c8447b82843e59c
Matt Krueger <matt@xceleratedconsulting.com>
2018-04-26 00:09:40 +0000

    Improved routing

--- config/routes.rb
+++ config/routes.rb
@@ -3,6 +3,8 @@ Rails.application.routes.draw do
   root to: 'pages#home' 
   # GET request to /about,  rails goes to pages controller, looks up 'about' action (AKA METHOD/FUNCTION)
   get 'about', to: 'pages#about'
-  resources :contacts
+  resources :contacts, only: :create
   get 'contact-us', to: 'contacts#new'
+  
+
 end
