762000f582b9183fbeaca390a749ea2c4c20e2f1
Matt Krueger <matt@xceleratedconsulting.com>
2018-04-27 03:19:06 +0000

    Added plans table

--- db/schema.rb
+++ db/schema.rb
@@ -10,7 +10,7 @@
 #
 # It's strongly recommended that you check this file into your version control system.
 
-ActiveRecord::Schema.define(version: 20180426202039) do
+ActiveRecord::Schema.define(version: 20180427031010) do
 
   create_table "contacts", force: :cascade do |t|
     t.string   "name"
@@ -18,6 +18,11 @@ ActiveRecord::Schema.define(version: 20180426202039) do
     t.text     "comments"
     t.datetime "created_at", null: false
     t.datetime "updated_at", null: false
+  end
+
+  create_table "plans", force: :cascade do |t|
+    t.string  "name"
+    t.decimal "price"
   end
 
   create_table "users", force: :cascade do |t|
