f3886ecbfe82d224320c2acf28bf990b20627472
Matt Krueger <matt@xceleratedconsulting.com>
2018-04-30 18:06:08 +0000

    Added routes for user profiles

--- config/routes.rb
+++ config/routes.rb
@@ -3,6 +3,10 @@ Rails.application.routes.draw do
   
   root to: 'pages#home' 
   devise_for :users, controllers: { registrations: 'users/registrations' }
+  resources :users do 
+    resource :profile
+  end
+  
   # GET request to /about,  rails goes to pages controller, looks up 'about' action (AKA METHOD/FUNCTION)
   get 'about', to: 'pages#about'
   resources :contacts, only: :create
