e82b9c7aee5f7f492c964b5d43aa6601552323ed
Matt Krueger <matt@xceleratedconsulting.com>
2018-05-01 23:38:10 +0000

    Added profile edit action and view

--- app/controllers/profiles_controller.rb
+++ app/controllers/profiles_controller.rb
@@ -21,6 +21,11 @@ class ProfilesController < ApplicationController
     end
   end
   
+  # GET to /users/:user_id/profile/edit
+  def edit 
+    @user = User.find(params[:user_id] )
+    @profile = @user.profile 
+  end
   
   
   private 
